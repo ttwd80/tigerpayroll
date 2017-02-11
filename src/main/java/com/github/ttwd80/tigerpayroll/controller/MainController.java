aa1df3d291ec24b48cdc620e687d0931be7ae60e
Titi Wangsa Damhore <ttwd80@gmail.com>
2017-02-11 12:45:43 +1100

    ok to display admin page based on admin role

--- src/main/java/com/github/ttwd80/tigerpayroll/controller/MainController.java
+++ src/main/java/com/github/ttwd80/tigerpayroll/controller/MainController.java
@@ -6,8 +6,7 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,16 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
-	@Autowired
-	@Qualifier("roleMap")
-	Map<String, String> roleMap;
+	@Value("#{roleLocationMap}")
+	Map<String, String> roleLocationMap;
 
 	@RequestMapping(value = "/main")
 	public ModelAndView get(final HttpServletRequest request, final HttpServletResponse response) {
-		final Set<String> set = roleMap.keySet();
+		final Set<String> set = roleLocationMap.keySet();
 		for (final String role : set) {
 			if (request.isUserInRole(role)) {
-				final String viewName = roleMap.get(role);
+				final String viewName = roleLocationMap.get(role);
 				return new ModelAndView(viewName);
 			}
 		}
