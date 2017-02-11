54bf82e03744c73666863959c3c9deedf5432610
Titi Wangsa Damhore <ttwd80@gmail.com>
2017-02-11 12:48:46 +1100

    test login for user and admin

--- src/test/java/com/github/ttwd80/tigerpayroll/web/LoginIT.java
+++ src/test/java/com/github/ttwd80/tigerpayroll/web/LoginIT.java
@@ -19,13 +19,24 @@ public class LoginIT extends AbstractSeleniumWebIT {
 	}
 
 	@Test
-	public void testLoginGood() {
+	public void testLoginAdminGood() {
 		webDriver.get(baseURL);
 		webDriver.findElement(By.id("username")).sendKeys("admin");
 		webDriver.findElement(By.id("password")).sendKeys("admin");
 		webDriver.findElement(By.id("submit")).click();
 		final String actual = webDriver.getCurrentUrl();
 		final String expected = "http://localhost:58080/admin/index";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLoginUserGood() {
+		webDriver.get(baseURL);
+		webDriver.findElement(By.id("username")).sendKeys("user");
+		webDriver.findElement(By.id("password")).sendKeys("user");
+		webDriver.findElement(By.id("submit")).click();
+		final String actual = webDriver.getCurrentUrl();
+		final String expected = "http://localhost:58080/user/index";
 		assertEquals(expected, actual);
 	}
 }
